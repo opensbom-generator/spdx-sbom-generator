f13c9b26182ecc8daf788f6b88de86f5e9f17085
Pratik K <pratikk@proximabiz.com>
2021-06-05 02:25:25 +0530

    extract metadata worker method added

---
+++ internal/modules/pip/worker/decoder.go
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package worker
+
+import (
+	"fmt"
+	"spdx-sbom-generator/internal/helper"
+	"spdx-sbom-generator/internal/models"
+	"strings"
+)
+
+var httpReplacer = strings.NewReplacer("https://", "", "http://", "")
+
+type GetPackageDetailsFunc = func(PackageName string) (string, error)
+
+type MetadataDecoder struct {
+	getPkgDetailsFunc GetPackageDetailsFunc
+}
+
+// New Metadata Decoder ...
+func NewMetadataDecoder(pkgDetailsFunc GetPackageDetailsFunc) *MetadataDecoder {
+	return &MetadataDecoder{
+		getPkgDetailsFunc: pkgDetailsFunc,
+	}
+}
+
+func SetMetadataValues(matadata *Metadata, datamap map[string]string) {
+	matadata.Name = datamap[KeyName]
+	matadata.Version = datamap[KeyVersion]
+	matadata.Description = datamap[KeySummary]
+	matadata.HomePage = httpReplacer.Replace(datamap[KeyHomePage])
+	matadata.Author = datamap[KeyAuthor]
+	matadata.AuthorEmail = datamap[KeyAuthorEmail]
+	matadata.License = datamap[KeyLicense]
+	matadata.Location = datamap[KeyLocation]
+
+	// Parsing "Requires"
+	if len(datamap[KeyRequires]) != 0 {
+		matadata.Modules = strings.Split(datamap[KeyRequires], ",")
+		for i, v := range matadata.Modules {
+			matadata.Modules[i] = strings.TrimSpace(v)
+		}
+	}
+}
+
+func ParseMetadata(metadata *Metadata, packagedetails string) {
+	pkgDataMap := make(map[string]string, 9)
+	resultlines := strings.Split(packagedetails, "\n")
+
+	for _, resline := range resultlines {
+		res := strings.Split(resline, ":")
+		if len(res) <= 1 {
+			continue
+		}
+		value := strings.TrimSpace(res[1])
+		// If there are more elements, then concatenate the second element onwards
+		// with a ":" in between
+		if len(res) > 2 {
+			for i := 2; i < len(res); i++ {
+				value += ":" + res[i]
+			}
+		}
+		pkgDataMap[strings.ToLower(res[0])] = value
+	}
+	SetMetadataValues(metadata, pkgDataMap)
+}
+
+func (d *MetadataDecoder) BuildMetadata(packagename string) Metadata {
+	var metadata Metadata
+
+	metadatastr, err := d.getPkgDetailsFunc(packagename)
+	if err != nil {
+		// If there was error fetching package details, we are setting all members to NOASSERTION.
+		// Except for Package Name
+		SetMetadataToNoAssertion(&metadata, packagename)
+	}
+	ParseMetadata(&metadata, metadatastr)
+
+	metadata.ProjectURL = BuildProjectUrl(metadata.Name, metadata.Version)
+	metadata.PackageURL = BuildPackageUrl(metadata.Name, metadata.Version)
+	if len(metadata.HomePage) > 0 {
+		metadata.PackageURL = metadata.HomePage
+	}
+	metadata.PackageJsonURL = BuildPackageJsonUrl(metadata.Name, metadata.Version)
+
+	metadata.DistInfoPath = BuildDistInfoPath(metadata.Location, metadata.Name, metadata.Version)
+	metadata.LocalPath = BuildLocalPath(metadata.Location, metadata.Name)
+	metadata.LicensePath = BuildLicenseUrl(metadata.Location, metadata.Name, metadata.Version)
+	metadata.MetadataPath = BuildMetadataPath(metadata.Location, metadata.Name, metadata.Version)
+
+	return metadata
+}
+
+func (d *MetadataDecoder) BuildModule(root bool, metadata Metadata) models.Module {
+	var module models.Module
+	module.Version = metadata.Version
+	module.Name = metadata.Name
+	module.Path = metadata.ProjectURL
+	module.LocalPath = metadata.LocalPath
+
+	contactType := models.Person
+	if IsAuthorAnOrganization(metadata.Author, metadata.AuthorEmail) {
+		contactType = models.Organization
+	}
+
+	// Prepare SupplierContact
+	// Todo : How to determine if this is person or organization
+	if len(metadata.AuthorEmail) > 0 {
+		module.Supplier = models.SupplierContact{
+			Type:  contactType,
+			Name:  metadata.Author,
+			Email: metadata.AuthorEmail,
+		}
+	}
+
+	module.PackageURL = metadata.PackageURL
+
+	// Todo : Get correct checksum
+	module.CheckSum = &models.CheckSum{
+		Algorithm: models.HashAlgoSHA1,
+		Value:     GetCheckSum(metadata.Name),
+	}
+
+	licensePkg, err := helper.GetLicenses(metadata.DistInfoPath)
+	if err == nil {
+		module.LicenseDeclared = helper.BuildLicenseDeclared(licensePkg.ID)
+		module.LicenseConcluded = helper.BuildLicenseConcluded(licensePkg.ID)
+		module.Copyright = helper.GetCopyright(licensePkg.ExtractedText)
+		module.CommentsLicense = licensePkg.Comments
+		if !helper.LicenseSPDXExists(licensePkg.ID) {
+			licensePkg.ID = fmt.Sprintf("LicenseRef-%s", licensePkg.ID)
+		}
+	}
+	module.PackageHomePage = metadata.HomePage
+	module.OtherLicense = []*models.License{} // How to get this
+	module.PackageComment = metadata.Description
+
+	// Python CLI command lists all non-root packages.
+	module.Root = root
+
+	// Todo : Nee to build sub-module details
+	module.Modules = map[string]*models.Module{}
+
+	return module
+}
+
+func (d *MetadataDecoder) GetMetadataList(pkgs []Packages) (map[string]Metadata, []Metadata) {
+	metainfo := map[string]Metadata{}
+	metaList := []Metadata{}
+
+	for _, pkg := range pkgs {
+		metadata := d.BuildMetadata(pkg.Name)
+		metaList = append(metaList, metadata)
+		metainfo[strings.ToLower(pkg.Name)] = metadata
+	}
+	return metainfo, metaList
+}
+
+func (d *MetadataDecoder) ConvertMetadataToModules(isRoot bool, pkgs []Packages, modules *[]models.Module) map[string]Metadata {
+	metainfo, metaList := d.GetMetadataList(pkgs)
+	for _, metadata := range metaList {
+		mod := d.BuildModule(isRoot, metadata)
+		*modules = append(*modules, mod)
+	}
+	return metainfo
+}
--- internal/modules/pip/worker/metadata.go
+++ internal/modules/pip/worker/metadata.go
@@ -2,15 +2,141 @@
 
 package worker
 
-import "encoding/json"
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"path"
+	"strings"
+)
+
+const ProjectUrl = "pypi.org/project"
+const PackageUrl = "pypi.org/pypi"
+const PackageDistInfoPath = ".dist-info"
+const PackageLicenseFile = ".dist-info/LICENSE"
+const PackageMetadataFie = ".dist-info/METADATA"
+
+// NOASSERTION constant
+const NoAssertion = "NOASSERTION"
+
+const (
+	KeyName        string = "name"
+	KeyVersion     string = "version"
+	KeySummary     string = "summary"
+	KeyHomePage    string = "home-page"
+	KeyAuthor      string = "author"
+	KeyAuthorEmail string = "author-email"
+	KeyLicense     string = "license"
+	KeyLocation    string = "location"
+	KeyRequires    string = "requires"
+)
+
+var AuthorAnOrganizationKeywords = []string{"Authority", "Team", "Developers", "Services", "Foundation", "Software"}
 
 type Packages struct {
 	Name    string `json:"name,omitempty"`
 	Version string `json:"version,omitempty"`
+}
+type Metadata struct {
+	Name           string
+	Version        string
+	Description    string
+	ProjectURL     string
+	PackageURL     string
+	PackageJsonURL string
+	HomePage       string
+	Author         string
+	AuthorEmail    string
+	License        string
+	DistInfoPath   string
+	LicensePath    string
+	MetadataPath   string
+	Location       string
+	LocalPath      string
+	Modules        []string
 }
 
 func LoadModules(data string) []Packages {
 	var _modules []Packages
 	json.Unmarshal([]byte(data), &_modules)
 	return _modules
+}
+
+func BuildProjectUrl(name string, version string) string {
+	paths := []string{ProjectUrl, name, version}
+	return path.Join(paths...)
+}
+
+func BuildPackageUrl(name string, version string) string {
+	paths := []string{PackageUrl, name, version}
+	return path.Join(paths...)
+}
+
+func BuildPackageJsonUrl(name string, version string) string {
+	paths := []string{PackageUrl, name, version, "json"}
+	return path.Join(paths...)
+}
+
+func BuildLocalPath(location string, name string) string {
+	paths := []string{location, name}
+	return path.Join(paths...)
+}
+
+func BuildDistInfoPath(location string, name string, version string) string {
+	package_name := name + "-" + version
+	package_metadata := package_name + PackageDistInfoPath
+	paths := []string{location, package_metadata}
+	return path.Join(paths...)
+}
+
+func BuildLicenseUrl(location string, name string, version string) string {
+	package_name := name + "-" + version
+	package_license := package_name + PackageLicenseFile
+	paths := []string{location, package_license}
+	return path.Join(paths...)
+}
+
+func BuildMetadataPath(location string, name string, version string) string {
+	package_name := name + "-" + version
+	package_metadata := package_name + PackageMetadataFie
+	paths := []string{location, package_metadata}
+	return path.Join(paths...)
+}
+
+func SetMetadataToNoAssertion(metadata *Metadata, packagename string) {
+	metadata.Name = packagename
+	metadata.Version = NoAssertion
+	metadata.Description = NoAssertion
+	metadata.HomePage = NoAssertion
+	metadata.Author = NoAssertion
+	metadata.AuthorEmail = NoAssertion
+	metadata.License = NoAssertion
+	metadata.LocalPath = NoAssertion
+	metadata.Modules = []string{}
+}
+
+// This is only a test code.
+// Need to find correct way of getting package checksum
+func GetCheckSum(content string) string {
+	h := sha1.New()
+	h.Write([]byte(content))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func IsAuthorAnOrganization(author string, authoremail string) bool {
+	// If both Author and Author-Email are defined as "None", we assume it as Organization
+	if (strings.Compare(strings.ToLower(author), "none") == 0) &&
+		(strings.Compare(strings.ToLower(authoremail), "none") == 0) {
+		return true
+	}
+
+	result := false
+	// If Author fields has any one of the keywords, we assume it as Organization
+	for _, v := range AuthorAnOrganizationKeywords {
+		if strings.Contains(strings.ToLower(author), strings.ToLower(v)) {
+			result = true
+			break
+		}
+	}
+	return result
 }
