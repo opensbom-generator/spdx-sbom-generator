6191a7f1cbaacb5d2f1aeea60dee78d72ed84585
prathapbproximabiz <prathapb@proximabiz.com>
2021-06-08 20:01:33 +0530

    few fixes from big java pom.xml reading

--- internal/modules/javamaven/errors.go
+++ internal/modules/javamaven/errors.go
@@ -8,6 +8,4 @@ import (
 
 type errType error
 
-var errDependenciesNotFound errType = errors.New("Unable to generate SPDX file, no modules or vendors found. Please install them before running spdx-sbom-generator, e.g.: `go mod vendor`")
-var errBuildlingModuleDependencies errType = errors.New("Error building modules dependencies")
-var errFailedToConvertModules errType = errors.New("Failed to convert modules")
+var errFailedToConvertModules errType = errors.New("failed to convert modules")
